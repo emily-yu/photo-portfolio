1af9aabf778ee840b451e50942d1fd3d4e225cd2
emily-yu <eyudeveloper@gmail.com>
2024-08-24 22:28:14 -0700

    add gallery widget

---
+++ src/app/_components/gallery.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { ParallaxScroll } from "./parallax-scroll";
+
+export function Gallery() {
+  return (
+    <>
+    <section>
+    <div style={{textAlign: "center", padding: "5%"}}>
+
+      <h2 className="text-5xl font-bold">Emily Yu</h2>
+      <div style={{textAlign: "center"}}>
+      <div>
+        <a href="/login">Login</a>
+        <a href="/gallery">Gallery</a>
+        <a href="/events">Events</a>
+        <a href="/contact">Contact</a>
+        </div>
+        <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">As an athlete turned photographer, I see with the players.</h4>
+        </div>
+        </div>
+        </section>
+  <ParallaxScroll images={images} />        </>
+
+);
+}
+
+const images = [
+    "/assets/gallery/bbc.jpg",
+    "/assets/gallery/bbc2.jpg",
+    "/assets/gallery/bbc3.jpg",
+    "/assets/gallery/bbc.jpg",
+    "/assets/gallery/bbc2.jpg",
+    "/assets/gallery/bbc3.jpg",
+    "/assets/gallery/bbc.jpg",
+    "/assets/gallery/bbc2.jpg",
+    "/assets/gallery/bbc3.jpg",
+    "/assets/gallery/bbc.jpg",
+    "/assets/gallery/bbc2.jpg",
+    "/assets/gallery/bbc3.jpg",
+];
---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/app/_components/container";
+import { Gallery } from "@/app/_components/gallery";
+
+export default function Index() {
+  
+    return (
+      <main>
+        <Container>
+          <Gallery/>
+        </Container>
+      </main>
+    );
+  }
+  
