89114a94b5968ea8ad1649c494e379f39b4d6d72
emily-yu <eyudeveloper@gmail.com>
2024-11-20 23:58:12 -0800

    use cdn image rendering

--- src/app/components/ui/parallax-scroll.tsx
+++ src/app/components/ui/parallax-scroll.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { CldImage } from "next-cloudinary";
 
 export const ParallaxScroll = ({
   images,
@@ -47,12 +48,13 @@ export const ParallaxScroll = ({
               style={{ y: translateFirst }} // Apply the translateY motion value here
               key={"grid-1" + idx}
             >
-              <Image
+              <CldImage
                 src={el}
                 className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
-                height="400"
-                width="400"
-                alt="thumbnail"
+                width="500"
+                height="500"
+                crop="fill" // This crops and fills the container
+                alt="Gallery Image"
               />
             </motion.div>
           ))}
@@ -60,12 +62,13 @@ export const ParallaxScroll = ({
         <div className="grid gap-10">
           {secondPart.map((el, idx) => (
             <motion.div style={{ y: translateSecond }} key={"grid-2" + idx}>
-              <Image
+              <CldImage
                 src={el}
                 className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
-                height="400"
-                width="400"
-                alt="thumbnail"
+                width="500"
+                height="500"
+                crop="fill" // This crops and fills the container
+                alt="Gallery Image"
               />
             </motion.div>
           ))}
@@ -73,14 +76,13 @@ export const ParallaxScroll = ({
         <div className="grid gap-10">
           {thirdPart.map((el, idx) => (
             <motion.div style={{ y: translateThird }} key={"grid-3" + idx}>
-              <Image
+              <CldImage
                 src={el}
                 className="h-80 w-full object-cover object-left-top rounded-lg gap-10 !m-0 !p-0"
-                height="400"
-                width="400"
-                alt="thumbnail"
-                objectFit="cover"
-                style={{ height: "100%", width: "100%" }}
+                width="500"
+                height="500"
+                crop="fill" // This crops and fills the container
+                alt="Gallery Image"
               />
             </motion.div>
           ))}
--- src/app/gallery/gallery.tsx
+++ src/app/gallery/gallery.tsx
@@ -7,31 +7,38 @@ interface QuoteProps {
   text: String;
 }
 export function Gallery() {
-  const images = [
-    "/assets/gallery/bbc.jpg",
-    "/assets/gallery/bbc2.jpg",
-    "/assets/gallery/bbc3.jpg",
-    "/assets/head/1.jpg",
-    "/assets/head/2.jpg",
-    "/assets/head/3.jpg",
-    "/assets/head/4.jpg",
-    "/assets/head/5.jpg",
-    "/assets/head/6.jpg",
-    "/assets/head/7.jpg",
-    "/assets/head/8.jpg",
+  // const images = [
+  //   "/assets/gallery/bbc.jpg",
+  //   "/assets/gallery/bbc2.jpg",
+  //   "/assets/gallery/bbc3.jpg",
+  //   "/assets/head/1.jpg",
+  //   "/assets/head/2.jpg",
+  //   "/assets/head/3.jpg",
+  //   "/assets/head/4.jpg",
+  //   "/assets/head/5.jpg",
+  //   "/assets/head/6.jpg",
+  //   "/assets/head/7.jpg",
+  //   "/assets/head/8.jpg",
+  // ];
+  const imagePublicIds = [
+    "cld-sample-5", 
+    "cld-sample-2", 
+    "cld-sample-3", 
+    "cld-sample-4",
+    "cld-sample-5", 
+    "cld-sample-2", 
+    "cld-sample-3", 
+    "cld-sample-4",
+    "cld-sample-5", 
+    "cld-sample-2", 
+    "cld-sample-3", 
+    "cld-sample-4",
   ];
+
   return (
     <>
       <Header />
-      <ParallaxScroll images={images} />
-      <CldImage
-          src="cld-sample-5" // Use this sample image or upload your own via the Media Explorer
-          width="500" // Transform the image: auto-crop to square aspect_ratio
-          height="500"
-          crop={{
-              type: 'auto',
-              source: true
-          }} alt={''}    />
+      <ParallaxScroll images={imagePublicIds} />
     </>
   );
 }
